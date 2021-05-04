49c6e4349dd855d743fa5ed87ac8153f6075dd35
fDevJc <fDevJc@gmail.com>
2021-05-04 22:04:10 +0900

    refactor: 프린트 메서드 추가

--- src/main/java/com/racingcar/ui/OutputUI.java
+++ src/main/java/com/racingcar/ui/OutputUI.java
@@ -4,17 +4,28 @@ public class OutputUI {
     public static void printExecutionResult() {
         System.out.println("실행결과");
     }
+
     public static void printInputCarNames(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
     public static void printInputMoveCycleCount(){
         System.out.println("시도할 회수는 몇회인가요?");
     }
-    public static void printCarDistance(String carName,int distance){
+
+    public static void printCarDistance(String carName,int distance) {
         StringBuilder distanceMark = new StringBuilder();
         for (int i = 0; i < distance; i++) {
             distanceMark.append("-");
         }
         System.out.printf("%s : %s%n", carName, distanceMark);
+    }
+
+    public static void printWinners(String maxDistanceCar) {
+        System.out.println(maxDistanceCar + "가 최종 우승했습니다.");
+    }
+
+    public static void print(String message) {
+        System.out.println(message);
     }
 }
